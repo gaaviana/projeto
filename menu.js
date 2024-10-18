6e7171324ee060a818b85ffb3885023cf3f663b5
Gabriel Viana <gaa.vianaa@gmail.com>
2024-10-17 21:54:47 -0300

    site novo

--- menu.js
+++ menu.js
@@ -1,12 +1,9 @@
-
-const botaoMenu = document.querySelector(".menu");
-const listaLinks = document.querySelector(".links-menu");
+const botaoMenu = document.querySelector(".menu-hamburguer");
+const listaLinks = document.querySelector(".menu");
 
 botaoMenu.addEventListener("click", function(){
     //Anulando o comportamento padrão de recarregar a pagina através de links
     // event.preventDefault();
-    
-
     listaLinks.classList.toggle("aberto")
 
     if (listaLinks.classList.contains("aberto")) {
